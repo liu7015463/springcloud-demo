2804acf929031624ba89708a090dbee8823aed72
liuyi <liuyiinhhu@qq.com>
2019-03-17 21:45:52 +0800

    4

---
+++ service-feign/src/main/java/com/ray/servicefeign/SchedualServiceHiHystric.java
@@ -0,0 +1,12 @@
+package com.ray.servicefeign;
+
+import com.ray.servicefeign.service.SchedualServiceHi;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry " + name;
+    }
+}
